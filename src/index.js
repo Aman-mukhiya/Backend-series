56ce2f09b05643e55b2437aedb07dc314edcc6c7
Aman Mukhiya <amanmukhiya4@gmail.com>
2025-01-27 20:31:32 +0545

    Handled ApiResponse and ApiError

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import connectDB from "./db/index.js"
 dotenv.config({
     path:'../env'
 })
-connectDB();
+connectDB()
+.then( () =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`⚙ Server is runnint at port : ${process.env.PORT}`);
+    } )
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!!! ", err)
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// This is know as Higher Order fuction 
+// i.e a function that accepts a function as parameter
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) => { () =>{} }
+// const asyncHandler = (func) => async () =>{} 
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }
