5209c29eff1f06ec9d5f84bf47e55240ac6f82ad
Aman Mukhiya <amanmukhiya4@gmail.com>
2025-02-16 20:57:04 +0545

    added all the routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import {
+         logoutUser,
+         loginUser, 
+         registerUser,
+         refreshAccessToken, 
+         updateUserAvatar, 
+         updateUserCoverImage 
+        } from "../controllers/user.controller.js";
+import { upload } fromverifyJWT,  "../middlewares/multer.middleware.js";
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
@@ -23,5 +30,12 @@ router.route("/login").post(loginUser)
 // seecured routes 
  router.route("/logout").post( verifyJWT, logoutUser)
  router.route("/refres-token").post(refreshAccessToken)
+ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+ router.route("/current-user").get(verifyJWT, getCurrentUser)
+ router.route("/update-accounts").patch(verifyJWT, updateAccountDetails)
+ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+ router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+ router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+ router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
